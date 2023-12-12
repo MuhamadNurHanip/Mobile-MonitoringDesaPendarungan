5b043c09468668d823bd32b00e3281755d361ca2
Hanif <nurhanif2312@gmail.com>
2023-12-12 21:54:19 +0700

    update

--- lib/pages/programkerja.dart
+++ lib/pages/programkerja.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:monitoringdesa_app/Widgets/AppHeader.dart';
 
 class Proker extends StatefulWidget {
-   Proker({Key? key}) : super(key: key);
+  Proker({Key? key}) : super(key: key);
 
   @override
   State<Proker> createState() => _ProkerState();
@@ -41,13 +41,6 @@ class _ProkerState extends State<Proker> {
               height: 10,
             ),
             Container(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Tuliskan deskripsi atau konten program kerja di sini...',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-            Container(
               margin: EdgeInsets.all(16.0),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.grey),
@@ -58,7 +51,7 @@ class _ProkerState extends State<Proker> {
                 child: DataTable(
                   columns: [
                     DataColumn(
-                      label: Text(
+                      label: Text(  
                         'No',
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
@@ -106,6 +99,151 @@ class _ProkerState extends State<Proker> {
                         DataCell(Container(
                           width: 100,
                           child: Text('Dana Umum'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Aksi yang dijalankan saat tombol di-klik
+                            },
+                            child: Text('Lihat'),
+                          ),
+                        )),
+                      ],
+                    ),
+                    DataRow(
+                      cells: [
+                        DataCell(Container(
+                          width: 50,
+                          child: Text('2'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('Hanif'),
+                        )),
+                        DataCell(Container(
+                          width: 150,
+                          child: Text('2023-12-11'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('Dana APBN'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Aksi yang dijalankan saat tombol di-klik
+                            },
+                            child: Text('Lihat'),
+                          ),
+                        )),
+                      ],
+                    ),
+                    DataRow(
+                      cells: [
+                        DataCell(Container(
+                          width: 50,
+                          child: Text('3'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('Pemberdayaan Perempuan'),
+                        )),
+                        DataCell(Container(
+                          width: 150,
+                          child: Text('2023-12-10'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('APBN'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Aksi yang dijalankan saat tombol di-klik
+                            },
+                            child: Text('Lihat'),
+                          ),
+                        )),
+                      ],
+                    ),
+                    DataRow(
+                      cells: [
+                        DataCell(Container(
+                          width: 50,
+                          child: Text('4'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('Desa Pintar'),
+                        )),
+                        DataCell(Container(
+                          width: 150,
+                          child: Text('2023-12-09'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('APBN'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Aksi yang dijalankan saat tombol di-klik
+                            },
+                            child: Text('Lihat'),
+                          ),
+                        )),
+                      ],
+                    ),
+                    DataRow(
+                      cells: [
+                        DataCell(Container(
+                          width: 50,
+                          child: Text('5'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('Pencegahan Stunting'),
+                        )),
+                        DataCell(Container(
+                          width: 150,
+                          child: Text('2023-12-08'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('APBN'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Aksi yang dijalankan saat tombol di-klik
+                            },
+                            child: Text('Lihat'),
+                          ),
+                        )),
+                      ],
+                    ),
+                    DataRow(
+                      cells: [
+                        DataCell(Container(
+                          width: 50,
+                          child: Text('6'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('Smart Kampung'),
+                        )),
+                        DataCell(Container(
+                          width: 150,
+                          child: Text('2023-12-01'),
+                        )),
+                        DataCell(Container(
+                          width: 100,
+                          child: Text('APBN'),
                         )),
                         DataCell(Container(
                           width: 100,
